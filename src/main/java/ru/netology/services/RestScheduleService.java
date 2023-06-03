5b46f5c03dac74d0c91e8d88d9d0d60c82e71432
Mike Lebedev <ley3@yandex.ru>
2023-06-03 20:21:13 +0300

    cheange

--- src/main/java/ru/netology/services/RestScheduleService.java
+++ src/main/java/ru/netology/services/RestScheduleService.java
@@ -8,7 +8,7 @@ public class RestScheduleService {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses - (money / 3 * 2);
+                money = (money - expenses) / 3;
             } else {
                 money = money + income - expenses;
             }
